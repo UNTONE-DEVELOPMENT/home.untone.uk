97eaa9f8103b5a4eafe04feac65fb806097390c9
Tanza3D <hubsterhub@gmail.com>
2024-02-12 23:00:28 +0000

    Update to add eref system

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
+<?php
+$eref = "";
+$erefs = [
+    "reddark" => "Reddark has now shut down."
+];
+
+if(isset($_GET['eref'])) {
+	if(in_array($_GET['eref'], $erefs)) {
+	    $eref = $erefs[$_GET['eref']];
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
+<meta name="title" content="UNTONE">
+<meta name="description" content="We make music, websites, games, and more.">
+<meta name="keywords" content="company,homepage,games,video games,music,record,record label,websites,development,it">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+
+
+<title>UNTONE</title>
     <link type="text/css" rel="stylesheet" href="https://id.untone.uk/css/global.css">
     <link type="text/css" rel="stylesheet" href="main.css">
     <link rel="stylesheet" type="text/css" href="https://shared.untone.uk/share.css">
@@ -15,6 +36,15 @@
 </head>
 
 <body class="theme_dark">
+    <?php
+    if($eref != "") {
+        ?>
+        <div class="eref-bar">
+            <p><?= $eref ?></p>
+        </div>
+        <?php
+    }
+    ?>
     <div class="cover">
         <img src="img/untone-icon.svg">
     </div>
@@ -57,7 +87,7 @@
             <div class="footer-right-row">
                     <a href="https://twitter.com/_UNTONE"><i class="fab fa-twitter"></i></a>
 
-                    <a href="https://twitter.com/_UNTONE"><img src="https://bsky.app/static/apple-touch-icon.png"></a>
+                    <a href="https://bsky.app/profile/untone.uk"><img src="https://bsky.app/static/apple-touch-icon.png"></a>
                 </div>
                 <div class="footer-right-row">
                     <!-- <a href="a">Contact</a> -->
